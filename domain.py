e0bec5b95ae71c0b6e211f6019e2b551ffd93f88
itMeansBigMountain <affan.fareed@gmail.com>
2023-05-01 01:12:56 -0500

    accurate domain name extractor

--- domain.py
+++ domain.py
@@ -1,3 +1,24 @@
 from urllib.parse import urlparse
 
 
+# get subdomain name   
+def get_subDomain(url):
+    try:
+        # (name.example.com) -> example.com
+        return urlparse(url).netloc
+    except:
+        return ""
+    
+
+
+# get subdomain name   
+def get_DomainName(url):
+    try:
+        results =  get_subDomain(url).split(".")
+        return results[-2] + "." +  results[-1]
+    except:
+        return ""
+    
+
+
+
